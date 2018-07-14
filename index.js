6b05de60786dab4c8ca7dde9a0b2048519e2f3a5
Evan Solomon <evanlsolomon@gmail.com>
2018-07-14 20:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
-var main = document.getElementbyID("main");
+var main = document.getElementById('main');
 main.remove();
+
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 var H1 = document.createElement('H1');
