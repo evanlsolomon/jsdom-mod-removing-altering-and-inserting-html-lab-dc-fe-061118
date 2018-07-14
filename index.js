07520900de312b9bb78fd12deafcbf373410768a
Evan Solomon <evanlsolomon@gmail.com>
2018-07-14 20:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
-
+document.getElementbyID = 'main';
+main.remove;
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+var H1 = document.createElement('H1');
+body.appendChild('H1');
+
+H1.innerHTML = ('Evan is the champion!');
