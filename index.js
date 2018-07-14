a2ba76aa99a0a6e1fc19562ba1cf9caed816f9b1
Evan Solomon <evanlsolomon@gmail.com>
2018-07-14 21:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ main.remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = h1 (id = 'victory', innerHTML = 'Evan is the champion');
-document.body.appendChild(newHeader);
 
-var myHeader = document.createElement("h1");
-myHeader.id = "victory";
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
 myHeader.innerHTML = ('Evan is the champion!');
 document.body.newHeader.appendChild(myHeader);
 
